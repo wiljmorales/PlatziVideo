e5be5458aeec331a581f2db496a7fa0fad63d61f
Wilfredo Morales <wiljmoralesa@gmail.com>
2020-05-23 20:31:49 -0400

    Se agrego PropTypes al proyecto

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/imgs/icons8-play-64.png';
 import plusIcon from '../assets/imgs/icons8-más-64.png';
@@ -12,9 +13,17 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
         <img className='carousel-item__details--icon' src={plusIcon} alt='' />
       </div>
       <p className='carousel-item__details--title'>{title}</p>
-      <p className='carousel-item__details--subtitle'>{`${year} ${contentRating} ${duration}`}</p>
+      <p className='carousel-item__details--subtitle'>{`${year} ${contentRating} ${duration} min`}</p>
     </div>
   </div>
 );
+
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+};
 
 export default CarouselItem;
